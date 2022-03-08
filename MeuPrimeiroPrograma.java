08666bd3e16c6c15135ab9b013da128013beae05
Letícia <lele.severino10@gmail.com>
2022-03-08 16:47:47 -0300

    commit

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println(" - Laboratório de Programação 2 - LP2\n - 2º ano de Informática\n - Seja bem vindo!\n - Vamos aprender Java.\n");
+    }
+    
+}
